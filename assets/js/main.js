b443bac9186e7b44aaf76ada47c292dc5df3306c
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-05-08 13:11:26 +0900

    2024.05.08

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 const menu = document.querySelector('.menu');
+const menu2 = document.querySelector(".")
 
 menu.addEventListener("click", function () {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
+        menu2.style.rigth = '-'
     } else {
         menu.classList.add('open');
+        menu2.style.rigth = '0'
     }
 })
