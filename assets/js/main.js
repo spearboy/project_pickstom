53b4868600e913c416279c7062b06b4f14c75c46
spearboy <tsagaanaa123@gmail.com>
2024-05-08 13:12:55 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 const menu = document.querySelector('.menu');
-const menu2 = document.querySelector(".")
+// const menu2 = document.querySelector(".")
 
 menu.addEventListener("click", function () {
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
-        menu2.style.rigth = '-'
+        // menu2.style.rigth = '-'
     } else {
         menu.classList.add('open');
-        menu2.style.rigth = '0'
+        // menu2.style.rigth = '0'
     }
 })
